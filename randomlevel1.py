0583134da948d5af97a7cea009db8cfc6a9279e6
meghanashastri1 <meghana360@gmail.com>
2017-08-14 16:13:21 -0700

    Create randomlevel1.py

---
+++ randomlevel1.py
@@ -0,0 +1,10 @@
+import random
+my_randoms=[]
+
+for i in range (10):
+    my_randoms.append(random.randrange(1,101,1))
+
+for elements in my_randoms:
+    if elements%5 == 0 
+        print (elements)
+#this prints a list of 10 random numbers and filters out and prints the multiples of 5 in that list
