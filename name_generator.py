41874f053b44d241534e51cd9069d8d8d4eabdd2
meghanashastri1 <meghana360@gmail.com>
2017-08-14 16:04:12 -0700

    Create name_generator.py

---
+++ name_generator.py
@@ -0,0 +1,8 @@
+First_name = ["Meghana", "Jacqueline", "Moy", "Mikayla", "Eliza", "Rozi", "Vanesa", "Hani", "Diana", "Kimberly", "Jolie", "Autumn", "Julia", "Alicia", "Khloe", "Seble", "Rose", "Julia", "Lynda" ]
+import random
+num = random.randint(0,len(First_name))
+Last_name = ["Shastri", "King", "Zhong", "Marchan", "Hidaru", "Marar", "Valley", "Ahmed", "Sykora-Lovaas", "Chang", "Tsegay", "Forrest", "Chen", "Kim", "Bucknor", "Baker", "Enyew", "Devine", "Pham"]
+num = random.randint(0,len(Last_name))
+print (num)
+print (First_name[num])
+print (Last_name[num])
